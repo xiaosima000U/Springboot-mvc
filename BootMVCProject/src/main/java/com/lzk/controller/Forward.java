edfac68372ac0faff8dc11fe324a03831f68d256
liuqiang <576110094@qq.com>
2019-12-02 23:24:39 +0800

    log4j

--- BootMVCProject/src/main/java/com/lzk/controller/Forward.java
+++ BootMVCProject/src/main/java/com/lzk/controller/Forward.java
@@ -1,14 +1,18 @@
 package com.lzk.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 @RequestMapping("/")
 public class Forward {
-
+    private static final Logger logger = LoggerFactory.getLogger(Forward.class);
     @RequestMapping("/index")
     public String index() {
+        logger.info("1234");
         System.out.println(1234);
         return "/index";
     }
